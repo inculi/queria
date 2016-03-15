5a30d48af8d49fded9c4a7def42367fe52029589
Manwholikespie <rdominguez@me.com>
2016-03-15 02:46:31 -0500

    Update queria.py
    Seems to be working fairly well. It is 2:46AM and I need to wake up around 10:00 or so... so I guess it is time for bed. Next, we'll add the ability to input your own question and answers and have it populate the lists accordingly. Until then, sleep sounds pretty nice.
    
    Goodnight.

--- queria.py
+++ queria.py
@@ -1,12 +1,14 @@
 # external libraries
-from bs4 import BeautifulSoup
 from google import search
-import fuzzywuzzy
+from bs4 import BeautifulSoup
 import requests
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
+import Levenshtein
 
 def getURLs(searchString):
     urls = []
-    for url in search(searchString, stop=20):
+    for url in search(searchString + " -filetype:pdf -filetype:docx -filetype:doc -filetype:pptx -filetype:ppt", stop=20):
         # Wikipedia returns multiple options under their links, each to a
         # different section of the same page... We only need one of these links.
         if "en.wikipedia.org/wiki/" and "#" not in url:
@@ -20,13 +22,35 @@ def parsePage(url):
     pageContent = soup.find_all("p") # find the text of the page
 
     for paragraph in pageContent:
-        print paragraph.text.replace("\n","")
-        print("\n") # break them up a little bit.
+        paragraphText = paragraph.text.replace("\n","")
+        # print(paragraphText)
+        # print fuzz.token_set_ratio(inputQuestion, paragraphText)
+        if int(fuzz.token_set_ratio(inputQuestion, paragraphText)) > 50:
+            # print paragraphText
+            paragraphTexts.append(paragraphText)
 
 # =========================   BEGIN MAIN FUNCTION   ============================
-searchURLs = getURLs("dung beetle")
+inputQuestion = "The majority of cases heard by federal courts begin in"
+answerAmount = 4
+answers = ["district courts", "state courts", "municipal courts", "appellate courts", "circuit courts"]
+searchURLs = getURLs(inputQuestion)
+
+paragraphTexts = []
 
 for url in searchURLs:
     print(url)
-    # parsePage(url)
+    parsePage(url)
+print("\n") # make some room
+
+scores = [0,0,0,0,0]
+
+for x in xrange(0,len(answers)):
+    for paragraph in paragraphTexts:
+        scores[x] += paragraph.count(answers[x])
+
+print("Scores:")
+for x in xrange(0,len(answers)):
+    print answers[x]
+    print scores[x]
+    print("\n")
 # ==========================   END MAIN FUNCTION   =============================
