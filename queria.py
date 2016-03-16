4aa384751d3c64a5c481c7de5b5c6263b68010ab
Manwholikespie <rdominguez@me.com>
2016-03-15 21:20:09 -0500

    Update queria.py
    Added the ability to answer free-response questions (aka non-multiple choice questions).
    
    Enjoy! :)

--- queria.py
+++ queria.py
@@ -7,13 +7,23 @@ from fuzzywuzzy import process
 import Levenshtein
 
 def getURLs(searchString):
-    urls = []
-    for url in search(searchString + " -filetype:pdf -filetype:docx -filetype:doc -filetype:pptx -filetype:ppt", stop=20):
-        # Wikipedia returns multiple options under their links, each to a
-        # different section of the same page... We only need one of these links.
-        if "en.wikipedia.org/wiki/" and "#" and "://books.google.com/books" and "www.youtube.com/" and "vimeo.com/" not in url:
-            urls.append(url)
-    return urls
+    if answerAmount == 0:
+        urls = []
+        for url in search(searchString + " site:quizlet.com", stop=3):
+            if len(urls) < 3: # The top three should suffice.
+                print(url)
+                urls.append(url)
+            else:
+                break
+        return urls
+    else:
+        urls = []
+        for url in search(searchString + " -filetype:pdf -filetype:docx -filetype:doc -filetype:pptx -filetype:ppt", stop=20):
+            # Wikipedia returns multiple options under their links, each to a
+            # different section of the same page... We only need one of these links.
+            if "en.wikipedia.org/wiki/" or "#" or "://books.google.com/books" or "www.youtube.com/" or "vimeo.com/" not in url:
+                urls.append(url)
+        return urls
 
 def parsePage(url):
     r = requests.get(url)
@@ -25,42 +35,80 @@ def parsePage(url):
         paragraphText = paragraph.text.replace("\n","")
         # print(paragraphText)
         # print fuzz.token_set_ratio(inputQuestion, paragraphText)
-        if int(fuzz.token_set_ratio(inputQuestion, paragraphText)) > 50:
+        if int(fuzz.token_set_ratio(inputQuestion.lower(), paragraphText.lower())) > 50:
             # print paragraphText
             paragraphTexts.append(paragraphText)
 
+def getCards(inputUrl):
+    questions = []
+    answers = []
+    # set up beautiful soup
+    r = requests.get(inputUrl)
+    soup = BeautifulSoup(r.content, "html.parser")
+
+    # pull term data
+    termTag = soup.find_all("span",{"class": "TermText qWord lang-en"})
+    for item in termTag:
+        questions.append(item.text)
+
+    defTag = soup.find_all("span",{"class": "TermText qDef lang-en"})
+    for item in defTag:
+        answers.append(item.text)
+
+    possibleAnswers = 0
+    for x in xrange(0,len(questions)):
+        if int(fuzz.token_set_ratio(inputQuestion.lower(), questions[x].lower())) > 90:
+            print questions[x]
+            print answers[x]
+            possibleAnswers += 1
+    if possibleAnswers == 0:
+        print("No possible answers found.")
+
 # =========================   BEGIN MAIN FUNCTION   ============================
+# ==========================   PRIMARY VARIABLES   =============================
+# VARIABLES FOR THE PURPOSE OF DEBUGGING...
+
 # inputQuestion = "The majority of cases heard by federal courts begin in"
-# answerAmount = 4
+# answerAmount = 5
 # answers = ["district courts", "state courts", "municipal courts", "appellate courts", "circuit courts"]
+
+# NORMALLY USED VARIABLES...
+
 inputQuestion = raw_input("What is your question?\n > ")
 answerAmount = int(raw_input("How many possible answers do you know?\n > "))
 
-answers = []
-for x in xrange(0,answerAmount):
-    answers.append(raw_input("Please input answer " + str(x+1) + "\n > "))
+if answerAmount == 0:
+    print("\nSearching...")
+    urls = getURLs(inputQuestion)
 
-# print(answers)
+    for url in urls:
+        getCards(url)
 
-print("\nSearching...")
-searchURLs = getURLs(inputQuestion)
+else:
+    answers = []
+    for x in xrange(0,answerAmount):
+        answers.append(raw_input("Please input answer " + str(x+1) + "\n > "))
+    # ========================   END PRIMARY VARIABLES   ===========================
 
-paragraphTexts = []
+    print("\nSearching...")
+    searchURLs = getURLs(inputQuestion)
 
-for url in searchURLs:
-    print(url)
-    parsePage(url)
-print("\n") # make some room
+    paragraphTexts = []
 
-scores = [0] * answerAmount
+    for url in searchURLs:
+        print(url)
+        parsePage(url)
+    print("\n") # make some room
 
-for x in xrange(0,len(answers)):
-    for paragraph in paragraphTexts:
-        scores[x] += paragraph.lower().count(answers[x].lower())
+    scores = [0] * answerAmount
 
-print("Scores:")
-for x in xrange(0,len(answers)):
-    print answers[x]
-    print scores[x]
-    print("\n")
+    for x in xrange(0,len(answers)):
+        for paragraph in paragraphTexts:
+            scores[x] += paragraph.lower().count(answers[x].lower())
+
+    print("Scores:")
+    for x in xrange(0,len(answers)):
+        print answers[x]
+        print scores[x]
+        print("\n")
 # ==========================   END MAIN FUNCTION   =============================
